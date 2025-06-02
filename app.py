adb8346004508f9765aae92a57725730ddda917b
Hoang Hiep <hoangdinhiep17022003@gmail.com>
2025-06-02 18:43:39 +0700

    Fix: create static dir to save QR image

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    url = "https://your-website-link.com"  # URL bạn muốn tạo QR
-    img = qrcode.make(url)
-    img_path = os.path.join('static', 'qr.png')
-    img.save(img_path)
-    return render_template('index.html', qr_image='qr.png')
+    # Tạo QR code
+    img = qrcode.make("https://heart-qr-flask.onrender.com")  # hoặc URL của bạn
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Đường dẫn lưu file QR vào thư mục static
+    img_path = "static/qr.png"
+    img.save(img_path)
+
+    return render_template("index.html", qr_image="qr.png")
